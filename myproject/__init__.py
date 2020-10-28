b99be882c4ac29744bf82046172c92d15f1954ef
Emir <emir.hajredini.99@gmail.com>
2020-10-28 22:09:18 +0100

    fixing database url

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -5,7 +6,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:emir1999@localhost/cityservice'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://postgres:emir1999@localhost/cityservice')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
